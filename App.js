90b499f2a2b5f182b2f57eccbc37ba20581c4571
Asaawari <61871556+Asaawari@users.noreply.github.com>
2020-09-22 18:24:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {AppTabNavigator} from './components/AppTabNavigator';
+import {AppDrawerNavigator} from './components/AppDrawerNavigator';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import SignUpLoginScreen from './screens/SignUpLoginScreen';
+
+export default class App extends React.Component {
+  render(){
+  return (
+    <AppContainer/>
+  );
+}
+}
+
+const SwitchNavigator = createSwitchNavigator({
+  SignUpLoginScreen : {screen : SignUpLoginScreen},
+  Drawer:{screen: AppDrawerNavigator},
+  BottomTab: {screen: AppTabNavigator},
+})
+
+const AppContainer = createAppContainer(SwitchNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
